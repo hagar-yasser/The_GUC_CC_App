b779bbd392cfbe736ed31a786f8b7ce252b3821d
hagar <hagaryasser642@gmail.com>
2021-12-21 10:32:12 +0200

    authorization and login/register page

---
+++ lib/objects/MyNames.dart
@@ -0,0 +1,13 @@
+class myNames {
+  static const usersTable = 'Users';
+  static const postsTable = 'Posts';
+  //EMAIL FIELD IN THE USERS TABLE
+  static const email = 'email';
+  //SAVEDPOSTSIDS FIELD IN THE USERS TABLE
+  static const savedPostsIDs = 'savedPostsIDs';
+  //NAME FIELD IN THE USERS TABLE
+  static const name = 'name';
+  //TYPE FIELD IN THE USERS TABLE
+  //TYPE OF THE USER CC OR NORMAL STUDENT
+  static const type = 'type';
+}
